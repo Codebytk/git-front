58e4ca26dceac214531b7333ca5ee13869faed55
Codebytk <ltk37733956@gmail.com>
2023-01-13 18:57:53 +0900

    no message

---
+++ 11/js/njs2.js
@@ -0,0 +1,3 @@
+document.write("njs.js는 njs2.js에 포함된");
+document.write("<div style='color:blue; font-size:16px;'>외부 자바 스크립트 파일</div>");
+document.write("<script src='./js/njs3.js'></script>");
---
+++ 11/js/njs3.js
@@ -0,0 +1,3 @@
+document.write("njs.js는 njs3.js에 포함된");
+document.write("<div style='color:green; font-size:16px;'>외부 자바 스크립트 파일</div>");
+alert("Nested Script File (내부 자바 스크립트에 포함)");
